47fee7315f496a3805140a688f437d610904d17b
Cheikh Tidiane KHOULE <49252781+ctkhoule@users.noreply.github.com>
2020-07-04 15:48:32 +0000

    Update CrudSQL.sql

--- CrudSQL.sql
+++ CrudSQL.sql
@@ -10,12 +10,16 @@ Create table Etudiant (
 );
 
 /* Insertion de données - (Create = INSERT) */
+    /* Insertion en spécifiant les colonnes à renseigner */
 INSERT into Etudiant(idEtudiant, nom, prenom, filiere, niveau) values
 (201901, 'KHOULE', 'Cheikh Tidiane', 'Génie informatique', 'Master 2');
+     /* Insertion dans toutes les colonnes */
 INSERT into Etudiant values(201902, 'FALL', 'Rama', 'Droit des affaires', 'Licence 1');
 
 /* Lecture de données - (Read = SELECT) */
+    /* Afficher tous les étudiants */
 SELECT * from Etudiant;
+    /* Afficher les noms et prénoms des étudiants de la licence 1 */
 SELECT nom, prenom from Etudiant where niveau = 'Licence 1';
 
 /* Mise à jour de données - (Update = UPDATE) */
